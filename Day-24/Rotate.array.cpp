f909ace7a40ee599a1f87cc96edcad23b07c3d5c
Sakthi <82046832+0xsakthi@users.noreply.github.com>
2022-02-07 22:49:37 +0530

    rotate the array from leetcode

---
+++ Day-24/Rotate.array.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        for(int i=0;i<k;i++){
+            nums.insert(nums.begin(),nums[nums.size()-1]);
+            nums.pop_back();
+        }
+    }
+};
